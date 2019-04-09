2419b27d2362188813cb84d6968cf18340221e06
Cherednyk Yaroslav <azazely85@gmail.com>
2019-04-09 14:37:49 +0300

    auth

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
+const passport = require('passport')
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 const categoryRoutes = require('./routes/category');
 const app = express();
-const keys = require('./config/keys')
+import keys from './config/keys';
+
 mongoose.connect(keys.mongoUri)
   .then(() => {
     console.log('mongo connected true');
@@ -19,6 +21,8 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/order', orderRoutes);
