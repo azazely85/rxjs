280cc64e6b8e856d24773fa493e9f36fb8c242b0
Cherednyk Yaroslav <azazely85@gmail.com>
2019-07-16 12:58:09 +0300

    add uploads

---
+++ middleware/upload.js
@@ -0,0 +1,30 @@
+import moment from 'moment';
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    const date = moment().format('DDMMYYYY-HHmmss_SSS');
+    cb(null, `${date}-${file.originalname}`);
+  }
+});
+
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimeType === 'image/png' || file.mimeType === 'image/jpeg') {
+    cb(null, true);
+  }
+  cb(null, false);
+};
+
+const limits = {
+  fileSize: 1024*1024*5
+};
+
+module.exports = multer({
+  storage,
+  fileFilter,
+  limits
+});
