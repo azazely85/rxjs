6845c8ec43a0e61285615c36103e3ed41e3d11fe
Cherednyk Yaroslav <azazely85@gmail.com>
2019-07-23 10:51:08 +0300

    fix order methods

--- controllers/order.js
+++ controllers/order.js
@@ -1,10 +1,46 @@
-const Order = require('../models/Order');
+import Order from '../models/Order';
+import errorHandler from "../utils/errorHandler";
 
-export function getAll(req, res) {
-
+export async function getAll(req, res) {
+  try {
+    const query = {
+      user: req.user.id
+    };
+    if (req.query.start) {
+      query.date = {
+        $gte: req.query.start
+      }
+    }
+    if (req.query.end) {
+      if (!query.date) {
+        query.date = {}
+      }
+      query.date['$lte'] = req.query.end
+    }
+  
+    if (req.query.order) {
+      query.order = +req.query.order
+    }
+    
+    const orders = await Order.find(query).sort({date: -1}).skip(+req.query.offset).limit(+req.query.limit);
+    res.status(200).json(orders)
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
-export function create(req, res) {
-
+export async function create(req, res) {
+  try {
+    const lastOrder = await Order.findOne({user: req.user.id}).sort({date: -1});
+    const maxOrder = lastOrder ? lastOrder.order : 0;
+    const order = await new Order({
+      order: maxOrder + 1,
+      list: req.body.list,
+      user: req.user.id
+    }).save();
+    res.status(201).json(order)
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
